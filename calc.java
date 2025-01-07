d799370e57519097ba1ae8e85a73e891f436b489
masterujjval <guptarickey3@gmail.com>
2025-01-07 15:34:26 +0530

    main file added:

---
+++ calc.java
@@ -0,0 +1,20 @@
+
+// Java program for simple calculator
+import java.io.*;
+import java.lang.*;
+import java.lang.Math;
+import java.util.Scanner;
+ 
+// Driver class
+public class BasicCalculator {
+    // main function
+    public static void main(String[] args)
+    {
+
+
+
+
+
+
+    }
+}
